b92e85c1c5010ec4670a378a9e7e2dc84420cc7f
Johnny <johncgumm@gmail.com>
2022-02-05 22:53:24 -0700

    fixed: modular for changing square size

--- CIS-269-Module-4-Assignment-2.cpp
+++ CIS-269-Module-4-Assignment-2.cpp
@@ -17,8 +17,177 @@ Run these functions for some large number of times, say 1,000, 10,000, or 1,000,
 */
 
 #include <iostream>
+#include <time.h>
+
+using namespace std;
+
+const int squareSize = 3;
+
+//Function Prototypes
+void randArray(int squareArray[squareSize][squareSize]);
+void displayArray(int squareArray[squareSize][squareSize]);
+double magicNumberCalculator(int squareArray[squareSize][squareSize]);
+bool rowSum(int squareArray[squareSize][squareSize], double num);
+bool colSum(int squareArray[squareSize][squareSize], double num);
+bool diagSum(int squareArray[squareSize][squareSize], double num);
 
 int main()
 {
+    // Initialize array
+    int square[squareSize][squareSize];
+
+    // Seed Randomizer
+    srand(time(NULL));
     
+    // Ask how many times to run
+    int timesToRun;
+    cout << "How many times would you like to run the program?" << endl;
+    cin >> timesToRun;
+    cout << endl;
+
+    // Declare vars used in each iteration
+    double magic;
+    bool row;
+    bool col;
+    bool diag;
+    int numOfMagicSquares = 0;
+
+    // Each iteration of squares
+    for (int i = 0; i < timesToRun; i++) {
+    
+        // Populate array with random numbers
+        randArray(square);
+
+        // Magic Number
+        magic = magicNumberCalculator(square);
+
+        // Row test
+        row = rowSum(square, magic);
+
+        // Column test
+        col = colSum(square, magic);
+
+        // Diagonal test
+        diag = diagSum(square, magic);
+
+        // Results
+        if (row && col && diag) {
+            displayArray(square);
+            cout << "Square is a magic square" << endl << endl;
+            numOfMagicSquares++;
+        }
+
+    }
+
+    cout << "Total magic squares:     " << numOfMagicSquares << endl;
+    cout << "Total non-magic squares  " << (timesToRun - numOfMagicSquares);
+
+    return 0;
+}
+
+void randArray(int squareArray[squareSize][squareSize]) 
+{
+    for (int i = 0; i < squareSize; i++) {
+        for (int j = 0; j < squareSize; j++) {
+            
+            squareArray[i][j] = (rand() % 9) + 1;
+        }
+    }
+}
+
+void displayArray(int squareArray[squareSize][squareSize])
+{
+    for (int i = 0; i < squareSize; i++) {
+        for (int j = 0; j < squareSize; j++) {
+
+            cout << squareArray[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+double magicNumberCalculator(int squareArray[squareSize][squareSize])
+{
+    double magicNumber = 0;
+
+    for (int i = 0; i < squareSize; i++) {
+        for (int j = 0; j < squareSize; j++) {
+
+            magicNumber += squareArray[i][j];
+        }
+    }
+
+    return (magicNumber/squareSize);
+}
+
+// Return true if all rows add to magic number
+bool rowSum(int squareArray[squareSize][squareSize], double num)
+{
+
+    double calc;
+
+    for (int i = 0; i < squareSize; i++) {
+
+        calc = 0;
+
+        for (int j = 0; j < squareSize; j++) {
+
+            calc += squareArray[i][j];
+        }
+
+        if (calc != num) { return false; }
+
+    }
+
+    return true;
+}
+
+// Return true if all columns add to magic number
+bool colSum(int squareArray[squareSize][squareSize], double num) 
+{
+    
+    double calc;
+
+    for (int i = 0; i < squareSize; i++) {
+
+        calc = 0;
+
+        for (int j = 0; j < squareSize; j++) {
+
+            calc += squareArray[j][i];
+        }
+
+        if (calc != num) return false;
+
+    }
+
+    return true;
+}
+
+bool diagSum(int squareArray[squareSize][squareSize], double num)
+{
+    
+    // Starting at [0][0]
+    double calc = 0;
+
+    for (int i = 0; i < squareSize; i++) {
+
+        calc += squareArray[i][i];
+
+    }
+
+    if (calc != num) return false;
+
+    calc = 0;
+
+    for (int i = 0; i < squareSize; i++) {
+        
+        calc += squareArray[i][squareSize - i];
+
+    }
+
+    if (calc != num) return false;
+
+    return true;
+
 }
